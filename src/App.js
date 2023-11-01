496c4e14149e1ec540c924bb20ad1e8ca123e4bb
ozdemireda02 <ozdemireda02@gmail.com>
2023-11-01 18:32:34 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import "./Pokecard.css"
+import Pokegame from './Pokegame';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Go Card Game</h1>
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
